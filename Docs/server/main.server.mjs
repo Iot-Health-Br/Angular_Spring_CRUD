12f7864216c01d616e34b0352537c7f9ba3f0abb
Iot-Health-Br <igl_macedo@hotmail.com>
2023-12-24 09:38:08 -0300

    Deploy

---
+++ Docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-55Q6ACOD.mjs";import"./chunk-Q3DJHPEN.mjs";import"./chunk-DZZULYD7.mjs";import"./chunk-4OGDPDRC.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
